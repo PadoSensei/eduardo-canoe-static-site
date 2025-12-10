91196b27bbddb067b8ac3955025883e636e43424
Sensei_P <padraigo.donoghue@gmail.com>
2025-12-10 00:56:35 +0000

    Update to config and redirect

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        book: resolve(__dirname, "book.html"),
-        faq: resolve(__dirname, "faq.html"),
-        tours: resolve(__dirname, "tours.html"),
-      },
-    },
-  },
 });
